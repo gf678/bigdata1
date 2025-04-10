bd65e775c695d6777be184c13f5a86864d086615
littlecode1112 <gf678@naver.com>
2025-04-10 10:54:47 +0900

    week06 ver1.1

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,18 +1,21 @@
-menus = ["아이스 아메리카노","카페라떼"]
-prices=[1500,2500]
+menus = ["아이스 아메리카노","카페라떼","커피"]
+prices=[1500,2500,1000]
+amount=[0,0,0]
 totalPrice=0
 while True:
-    menu = input (f"1){menus[0]} {prices[0]} 2){menus[1]} {prices[1]} 3)주문 종료")
-    if menu == "1":
-        print(f"{menus[0]}를 주문하셨습니다. 가격은 {prices[0]}원 입니다.")
-        totalPrice = totalPrice+prices[0]
-    elif menu == "2":
-        print(f"{menus[1]}를 주문하셨습니다. 가격은 {prices[1]}원 입니다.")
-        totalPrice = totalPrice + prices[1]
-    elif menu == "3":
-        print("종료합니다.")
-        break
-    else :
-        print(f"{menus}번은 없는 메뉴입니다.")
+    menu = input(f"1) {menus[0]} {prices[0]}원  2) {menus[1]} {prices[1]}원  3) {menus[2]} {prices[2]}원  4) 주문 종료 >> ")
 
+    if menu in ["1", "2", "3"]:
+        index = int(menu) - 1
+        print(f"{menus[index]}를 주문하셨습니다. 가격은 {prices[index]}원 입니다.")
+        totalPrice += prices[index]
+        amount[index] += 1
+    elif menu == "4":
+        print("주문을 종료합니다.")
+        break
+    else:
+        print(f"{menu}번은 없는 메뉴입니다.")
+
+for i in range(len(menus)):
+    print(f"{menus[i]} {prices[i]}원 {amount[i]}잔 총 {prices[i]*amount[i]}잔")
 print(f"총 가격:{totalPrice}")
