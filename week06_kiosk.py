4581fe57d4a25bb0d8d2d5e7ffe7533131b9b7eb
littlecode1112 <gf678@naver.com>
2025-04-10 12:17:56 +0900

    week06 ver1.3

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,35 +1,46 @@
-drinks = ["아이스 아메리카노","카페라떼","커피","카푸치노"]
-prices=[1500,2500,1000,3000]
-amount=[0,0,0,0]
+def get_menu_text(drinks, prices):
+    menu_text = ""
+    for i in range(len(drinks)):
+        menu_text += f"{i+1}) {drinks[i]} {prices[i]}원  "
+    menu_text += f"{len(drinks)+1}) 주문 종료 >> "
+    return menu_text
+
+
+def choice_menu(menu, totalPrice):
+    if menu.isdigit():  # 숫자인지 확인
+        num = int(menu)
+        if 1 <= num <= len(drinks):
+            index = num - 1
+            amount[index] += 1
+            print(f"{drinks[index]}를 주문하셨습니다. 가격은 {prices[index]}원 입니다. {amount[index]}개 주문하셨습니다.\n")
+            totalPrice += prices[index]
+            return totalPrice, False  # 계속 주문
+        elif num == len(drinks) + 1:
+            print("주문을 종료합니다.")
+            return totalPrice, True  # 주문 종료
+        else:
+            print(f"{menu}번은 없는 메뉴입니다.")
+    else:
+        print("숫자로 입력해주세요.")
+
+    return totalPrice, False
+
+drinks = ["아이스 아메리카노","카페 라떼","커피","카푸치노","샌즈 커피"]
+prices=[1500,2500,1000,3000,5000]
+amount = [0] * len(drinks)
 totalPrice=0
 menu_text = ""
 
+
 while True:
-    menu_text = "" #문자열 초기화
-    for i in range(len(drinks)): # 음료 개수만큼의 메뉴 띄우기 반복
-        menu_text += f"{i+1}) {drinks[i]} {prices[i]}원  "
-    menu_text += f"{len(drinks)+1}) 주문 종료 >> " #마지막 종료 띄우기
-
-    menu = input(menu_text) #숫자넣기
-
-    if menu.isdigit(): #숫자 맞어? 검출기
-        num = int(menu) #문자열을 정수형으로 변환
-        if 1 <= num <= len(drinks):
-            index = num - 1
-            print(f"{drinks[index]}를 주문하셨습니다. 가격은 {prices[index]}원 입니다.")
-            totalPrice += prices[index]
-            amount[index] += 1  #음료 개수만큼의 메뉴 리뷰
-        elif num == len(drinks) + 1:
-            print("주문을 종료합니다.")
-            break   #주문종료
-        else:
-            print(f"{menu}번은 없는 메뉴입니다.") #잘못고름
-    else:
-        print("숫자로 입력해주세요.")    #숫자아님
-
+    menu = input(get_menu_text(drinks,prices)) #숫자넣기
+    totalPrice, is_done = choice_menu(menu, totalPrice)
+    if is_done:
+        break
+print(f"{'상품명':^20}{'단가':^6}{'수량':^6}{'금액':^10}")
 for i in range(len(drinks)):
     if amount[i] <=0:
         pass
     else:
-        print(f"{drinks[i]} {prices[i]}원 {amount[i]}잔 총 {prices[i]*amount[i]}잔")
+        print(f"{drinks[i]:^20}{prices[i]:^6}{amount[i]:^6}{prices[i]*amount[i]:^10}")
 print(f"총 가격:{totalPrice}")
