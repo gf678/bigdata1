25ec86fd46136f135a8164173151b44fd86bc505
littlecode1112 <gf678@naver.com>
2025-04-10 11:21:51 +0900

    week06 ver1.2

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,21 +1,35 @@
-menus = ["아이스 아메리카노","카페라떼","커피"]
-prices=[1500,2500,1000]
-amount=[0,0,0]
+drinks = ["아이스 아메리카노","카페라떼","커피","카푸치노"]
+prices=[1500,2500,1000,3000]
+amount=[0,0,0,0]
 totalPrice=0
+menu_text = ""
+
 while True:
-    menu = input(f"1) {menus[0]} {prices[0]}원  2) {menus[1]} {prices[1]}원  3) {menus[2]} {prices[2]}원  4) 주문 종료 >> ")
+    menu_text = "" #문자열 초기화
+    for i in range(len(drinks)): # 음료 개수만큼의 메뉴 띄우기 반복
+        menu_text += f"{i+1}) {drinks[i]} {prices[i]}원  "
+    menu_text += f"{len(drinks)+1}) 주문 종료 >> " #마지막 종료 띄우기
 
-    if menu in ["1", "2", "3"]:
-        index = int(menu) - 1
-        print(f"{menus[index]}를 주문하셨습니다. 가격은 {prices[index]}원 입니다.")
-        totalPrice += prices[index]
-        amount[index] += 1
-    elif menu == "4":
-        print("주문을 종료합니다.")
-        break
+    menu = input(menu_text) #숫자넣기
+
+    if menu.isdigit(): #숫자 맞어? 검출기
+        num = int(menu) #문자열을 정수형으로 변환
+        if 1 <= num <= len(drinks):
+            index = num - 1
+            print(f"{drinks[index]}를 주문하셨습니다. 가격은 {prices[index]}원 입니다.")
+            totalPrice += prices[index]
+            amount[index] += 1  #음료 개수만큼의 메뉴 리뷰
+        elif num == len(drinks) + 1:
+            print("주문을 종료합니다.")
+            break   #주문종료
+        else:
+            print(f"{menu}번은 없는 메뉴입니다.") #잘못고름
     else:
-        print(f"{menu}번은 없는 메뉴입니다.")
+        print("숫자로 입력해주세요.")    #숫자아님
 
-for i in range(len(menus)):
-    print(f"{menus[i]} {prices[i]}원 {amount[i]}잔 총 {prices[i]*amount[i]}잔")
+for i in range(len(drinks)):
+    if amount[i] <=0:
+        pass
+    else:
+        print(f"{drinks[i]} {prices[i]}원 {amount[i]}잔 총 {prices[i]*amount[i]}잔")
 print(f"총 가격:{totalPrice}")
