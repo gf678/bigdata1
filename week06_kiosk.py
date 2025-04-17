92606478ab0112b69bd54590514730e414b33f8c
littlecode1112 <gf678@naver.com>
2025-04-17 11:35:07 +0900

    week7 update 2

--- week06_kiosk.py
+++ week06_kiosk.py
@@ -1,4 +1,4 @@
-def get_menu_text(drinks, prices):
+def get_menu_text(drinks , prices):
     #menu_text = ""
     # for i in range(len(drinks)):
     #     menu_text += f"{i+1}) {drinks[i]} {prices[i]}원  "
@@ -8,16 +8,16 @@ def get_menu_text(drinks, prices):
     return menu_text
 
 
-def choice_menu(menu, totalPrice):
+def choice_menu(menu: int, totalPrice :int ) ->tuple[int, bool]:
     if menu.isdigit():  # 숫자인지 확인
-        num = int(menu)
-        if 1 <= num <= len(drinks):
-            index = num - 1
+        nMenu = int(menu)
+        if 1 <= nMenu <= len(drinks):
+            index = nMenu - 1
             amount[index] += 1
             print(f"{drinks[index]}를 주문하셨습니다. 가격은 {prices[index]}원 입니다. {amount[index]}개 주문하셨습니다.\n")
             totalPrice += prices[index]
             return totalPrice, False  # 계속 주문
-        elif num == len(drinks) + 1:
+        elif nMenu == len(drinks) + 1:
             print("주문을 종료합니다.")
             return totalPrice, True  # 주문 종료
         else:
@@ -26,6 +26,15 @@ def choice_menu(menu, totalPrice):
         print("숫자로 입력해주세요.")
 
     return totalPrice, False
+
+def result_Receipt(totalPrice):
+    print(f"{'상품명':^20}{'단가':^6}{'수량':^6}{'금액':^10}")
+    for i in range(len(drinks)):
+        if amount[i] <= 0:
+            pass
+        else:
+            print(f"{drinks[i]:^20}{prices[i]:^6}{amount[i]:^6}{prices[i] * amount[i]:^10}")
+    print(f"총 가격:{totalPrice}")
 
 drinks = ["아이스 아메리카노","카페 라떼","커피","카푸치노","샌즈 커피"]
 prices=[1500,2500,1000,3000,5000]
@@ -40,10 +49,4 @@ while True:
     if is_done:
         break
 
-print(f"{'상품명':^20}{'단가':^6}{'수량':^6}{'금액':^10}")
-for i in range(len(drinks)):
-    if amount[i] <=0:
-        pass
-    else:
-        print(f"{drinks[i]:^20}{prices[i]:^6}{amount[i]:^6}{prices[i]*amount[i]:^10}")
-print(f"총 가격:{totalPrice}")
+result_Receipt(totalPrice)
